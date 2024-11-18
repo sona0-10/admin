33385a6fef9e928d48b1e03812a6562799eb8754
sona shaji <126900581+sona0-10@users.noreply.github.com>
2024-11-18 06:27:35 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <main className='main-container'>
+     <div className='main-title'>
+    <h3>DASHBOARD</h3>
+    </div>
+    <div></div>
+    </main>
+  )
+}
+
+export default Home
